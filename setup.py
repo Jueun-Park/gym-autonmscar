c3c30fd90d0475d9e1db46acb002a4b8cf17f19a
Jueun <elpion19@gmail.com>
2019-07-04 17:40:10 +0900

    add pillow

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from setuptools import setup
 
 setup(name='gym_autonmscar',
       version='0.0.1',
-      install_requires=['gym', 'pygame', 'tensorflow', 'stable-baselines']
+      install_requires=['gym',
+                        'pygame',
+                        'tensorflow',
+                        'stable-baselines',
+                        'Pillow',
+                        ]
       )
