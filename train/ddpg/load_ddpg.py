01e32a6eb8c413e76e0195080deb986bca8ca87c
Jueun-Park <elpion19@gmail.com>
2019-07-05 18:03:51 +0900

    check playing fps

--- train/ddpg/load_ddpg.py
+++ train/ddpg/load_ddpg.py
@@ -1,20 +1,45 @@
+import csv
+import threading
+import os
 import gym
 import gym_autonmscar
-import os
 from stable_baselines import DDPG
 
-# TODO: use argparse
-model_name = "/ddpg-models/ddpg-model_3000.pkl"
+model_name = "/ddpg-models2/ddpg-model_567000.pkl"
+fps = 0
+seconds = 0
 
-env = gym.make('autonmscarContinuous-v0')
-model = DDPG.load(os.path.dirname(
-    os.path.realpath(__file__)) + model_name)
+def get_fps():
+    global fps, seconds, writer
+    print("step per second: ", fps)
+    if seconds == 0:
+        writer.writerow(["seconds", "fps"])
+    else:
+        writer.writerow([seconds, fps])
+    fps = 0
+    seconds += 1
+    threading.Timer(1, get_fps).start()
 
-obs = env.reset()
-done = False
-for i in range(1000):
-    if done:
-        env.reset()
-    action, _states = model.predict(obs)
-    obs, rewards, done, info = env.step(action)
-    env.render()
+
+if __name__ == "__main__":
+    env = gym.make('autonmscarContinuous-v0')
+    model = DDPG.load(os.path.dirname(
+        os.path.realpath(__file__)) + model_name)
+
+    output_file = open('fps_ddpg_play.csv', 'w', encoding='utf-8')
+    global writer
+    writer = csv.writer(output_file)
+    fps_thread = threading.Thread(target=get_fps)
+    fps_thread.start()
+
+    obs = env.reset()
+    done = False
+    for i in range(1000):
+        if done:
+            env.reset()
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        env.render()
+        fps += 1
+    
+    output_file.close()
--- train/dqn/load_dqn.py
+++ train/dqn/load_dqn.py
@@ -1,19 +1,45 @@
+import csv
+import threading
+import os
 import gym
 import gym_autonmscar
-import os
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import DQN
 
-# TODO: use argparse
 model_name = "/dqn-models/dqn-model_544000.pkl"
+fps = 0
+seconds = 0
 
-env = gym.make('autonmscar-v0')
-env = DummyVecEnv([lambda: env])
-model = DQN.load(os.path.dirname(
-    os.path.realpath(__file__)) + model_name)
 
-obs = env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
+def get_fps():
+    global fps, seconds, writer
+    print("step per second: ", fps)
+    if seconds == 0:
+        writer.writerow(["seconds", "fps"])
+    else:
+        writer.writerow([seconds, fps])
+    fps = 0
+    seconds += 1
+    threading.Timer(1, get_fps).start()
+
+
+if __name__ == "__main__":
+    env = gym.make('autonmscar-v0')
+    env = DummyVecEnv([lambda: env])
+    model = DQN.load(os.path.dirname(
+        os.path.realpath(__file__)) + model_name)
+
+    output_file = open('fps_dqn_play.csv', 'w', encoding='utf-8')
+    global writer
+    writer = csv.writer(output_file)
+    fps_thread = threading.Thread(target=get_fps)
+    fps_thread.start()
+
+    obs = env.reset()
+    for i in range(1000):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
+        fps += 1
+
+    output_file.close()
